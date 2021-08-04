6c1f7eeaaef3154a557374a28925d5947c63ac11
Andrew Hartness <hartness.andrew@gmail.com>
2021-08-04 18:37:02 -0400

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,9 +21,8 @@ const Footer = ({ data }) => {
 
           <ul className="copyright">
             <li>
-              Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
-                PAPA
+              <a title="Source Code" href="https://github.com/ahartness/hartness-portfolio">
+                Source Code
               </a>
             </li>
           </ul>
